1f7ff799c6d910e807b7df3341b40231e5cbbcf7
Anastasiya Makaronok <>
2023-08-10 14:14:36 +0200

    Creating new text project

---
+++ myTest/myTest/myTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  myTestApp.swift
+//  myTest
+//
+//  Created by Anastasiya Makaronok on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct myTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
